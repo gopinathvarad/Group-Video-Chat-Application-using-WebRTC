718e772789b3319ec6574e3eb637d98a10f5d814
Gopinath <gvarad2001@gmail.com>
2024-06-03 23:04:36 +0100

    Completed Introduction Page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import IntroductionPage from "./IntroductionPage/IntroductionPage";
 import JoinRoomPage from "./JoinRoomPage/JoinRoomPage";
