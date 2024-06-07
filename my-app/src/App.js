ca3b3b58819c9486197fa7970731c87dced92630
Gopinath <gvarad2001@gmail.com>
2024-06-07 16:13:56 +0100

    updated app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,10 +3,15 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import IntroductionPage from "./IntroductionPage/IntroductionPage";
 import JoinRoomPage from "./JoinRoomPage/JoinRoomPage";
 import RoomPage from "./RoomPage/RoomPage";
+import { connectWithSocketIOServer } from "./utils/wss";
 
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    connectWithSocketIOServer();
+  }, []);
+
   return (
     <Router>
       <Switch>
